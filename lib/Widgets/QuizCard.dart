ea0b7669dd07fb5eb8f533ac804c0371cbba55c0
imransihab0 <imran.sihab.0@gmail.com>
2024-03-02 12:39:25 +0600

    Quiz part updated

---
+++ lib/Widgets/QuizCard.dart
@@ -0,0 +1,165 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+Color BaseColor = Color(0xFF1D458B);
+TextStyle Style = TextStyle(fontSize: 21,fontWeight: FontWeight.bold,color: BaseColor);
+
+class QuizCard extends StatefulWidget {
+  final TextEditingController controller;
+  final List info;
+  final Function() ButtonClick;
+  const QuizCard({super.key, required this.info, required this.ButtonClick, required this.controller});
+
+  @override
+  State<QuizCard> createState() => _QuizCardState(this.info,this.ButtonClick);
+}
+
+class _QuizCardState extends State<QuizCard> {
+  List info;
+  Function() ButtonClick;
+  bool click1 = false, click2 = false, click3 = false, click4 = false;
+
+
+  _QuizCardState(this.info, this.ButtonClick);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final String question = info[0];
+
+    String qstn = question;
+
+    // For center whitebox
+    Center MidBox = Center(
+      child: Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(2))
+          ),
+
+        ),
+      ),
+    );
+
+    void clickfun1()
+    {
+      setState(() {
+        click1 = true;
+        click2 = false;
+        click3 = false;
+        click4 = false;
+        widget.controller.text = '1';
+      });
+    }
+
+    void clickfun2()
+    {
+      setState(() {
+        click1 = false;
+        click2 = true;
+        click3 = false;
+        click4 = false;
+        widget.controller.text = '2';
+      });
+    }
+
+    void clickfun3()
+    {
+      setState(() {
+        click1 = false;
+        click2 = false;
+        click3 = true;
+        click4 = false;
+        widget.controller.text = '3';
+      });
+    }
+
+    void clickfun4()
+    {
+      setState(() {
+        click1 = false;
+        click2 = false;
+        click3 = false;
+        click4 = true;
+        widget.controller.text = '4';
+      });
+    }
+
+
+    List<InkWell> Quizbuttons = [];
+    List func = [clickfun1, clickfun2, clickfun3, clickfun4];
+    List<bool> clicked = [click1,click2,click3,click4];
+    List<String> OptionText = [info[1],info[2],info[3],info[4]];
+
+    Row BottomButton = Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        ElevatedButton(onPressed: ButtonClick,
+            style : ElevatedButton.styleFrom(
+              backgroundColor: BaseColor,
+              elevation: 4,
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            child: Text(info[5],style: Style.copyWith(fontSize: 18,color: Colors.white),))
+      ],);
+
+    for(int i=0; i<4; i++)
+    {
+      Quizbuttons.add(InkWell(
+        onTap: func[i],
+        child: Row(
+          children: [
+            Container(
+              height: 22,
+              width: 22,
+              decoration: BoxDecoration(
+                  color: BaseColor,
+                  borderRadius: BorderRadius.all(Radius.circular(2))
+              ),
+              child: clicked[i]?Center(child: Icon(Icons.check, color: Colors.white,size: 20,)) : MidBox,
+            ),
+            SizedBox(width: 15,),
+            Text(OptionText[i], style: Style.copyWith(fontSize: 18),)
+          ],
+        ),
+      ));
+    }
+
+
+
+    return Container(
+      height: 320,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          boxShadow: [BoxShadow(blurRadius: 20,offset: Offset(1,3),color: Colors.black87.withOpacity(.7)),]
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 25,right: 25,top: 10,bottom: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(qstn, style: Style,),
+            SizedBox(
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: Quizbuttons,
+              ),
+            ),
+            BottomButton
+          ],),
+      ),
+    );
+  }
+}
