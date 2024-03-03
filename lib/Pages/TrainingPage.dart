3ec30fe2bfd87f063dab94578ae8b2cf100c2003
Muntasir <md.muntasir2700@gmail.com>
2024-03-03 21:28:01 +0600

    Training page updated

--- lib/Pages/TrainingPage.dart
+++ lib/Pages/TrainingPage.dart
@@ -40,22 +40,24 @@ class TrainingPage extends StatefulWidget {
 class _TrainingPageState extends State<TrainingPage> {
   List<QuizCard> quizes = [];
   bool isPressed = false;
-  double quizH = 400;
-
+  double quizH = 400,rnd = 5;
   int time = 600;
 
   @override
   Widget build(BuildContext context) {
     double h = MediaQuery.of(context).size.height,
-        w = MediaQuery.of(context).size.width,
-        quizW = w * .9;
+          w = MediaQuery.of(context).size.width,quizW = w*.9;
+
 
     void QuizButton() {
       setState(() {
         isPressed = true;
         quizH = h * .75;
+        rnd = 50;
       });
     }
+
+
 
     for (var element in QuizText.Quizes) {
       // element[0] = "${i.toString()}. ${element[0]}";
@@ -65,31 +67,39 @@ class _TrainingPageState extends State<TrainingPage> {
           controller: TextEditingController()));
     }
 
-    AnimatedContainer quizPage = AnimatedContainer(
+    Container quizFrontpage = Container(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xff0a8144),
+          elevation: 4,
+          padding: const EdgeInsets.symmetric(
+              horizontal: 30, vertical: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+        ),
+        onPressed: QuizButton,
+        child: const Text("Start Quiz",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            )),
+      ),
+    );
+
+
+    AnimatedContainer QuizWidget = AnimatedContainer(
         duration: Duration(milliseconds: time),
         height: quizH - 50,
+        width: quizW,
+
         child: SingleChildScrollView(
             child: isPressed
                 ? Column(
                     children: quizes,
                   )
-                : ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xff0a8144),
-                      elevation: 4,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30, vertical: 10),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                    onPressed: QuizButton,
-                    child: const Text("Start Quiz",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        )),
-                  )));
+                : quizFrontpage)
+    );
 
     return Scaffold(
       backgroundColor: const Color(0xFF1D458B),
@@ -134,8 +144,12 @@ class _TrainingPageState extends State<TrainingPage> {
               duration: Duration(milliseconds: time),
               height: quizH,
               width: quizW,
-              color: const Color(0xFF4d76bf).withOpacity(.5),
+
               // decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(rnd),topRight: Radius.circular(rnd)),
+                  color: const Color(0xFF4d76bf).withOpacity(.5),
+              ),
               child: Column(
                 children: [
                   const Center(
@@ -147,8 +161,9 @@ class _TrainingPageState extends State<TrainingPage> {
                       color: Colors.white,
                     ),
                   )),
+                  SizedBox(height: 10,),
                   // Image(image: AssetImage('assets/images/quiz.jpg')),
-                  quizPage,
+                  QuizWidget,
                 ],
               ),
             ),
