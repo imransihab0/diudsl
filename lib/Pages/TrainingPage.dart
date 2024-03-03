d511002fe67dbb9c71dc424e94cf1097c06ac63f
imransihab0 <imran.sihab.0@gmail.com>
2024-03-04 00:40:57 +0600

    Design Upgraded

--- lib/Pages/TrainingPage.dart
+++ lib/Pages/TrainingPage.dart
@@ -1,10 +1,7 @@
-import 'dart:ui';
 import 'package:diudsl/Contents/Quiz%20Contents.dart';
 import 'package:diudsl/Widgets/CustomInfoContainer.dart';
 import 'package:diudsl/Widgets/QuizCard.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CustomDesign extends CustomClipper<Path> {
   @override
@@ -116,7 +113,7 @@ class _TrainingPageState extends State<TrainingPage> {
           Image.asset("assets/images/quiz.png"),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xff0a8144),
+              backgroundColor: const Color(0xff0a8144),
               elevation: 4,
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
               shape: RoundedRectangleBorder(
@@ -143,19 +140,18 @@ class _TrainingPageState extends State<TrainingPage> {
         ElevatedButton(
             onPressed: resultFunction,
             style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xff0a8144),
+              backgroundColor: const Color(0xff0a8144),
               elevation: 4,
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(5),
               ),
             ),
-            child: Text(
+            child: const Text(
               "See Result",
-              style: TextStyle(
-                  fontSize: 16, color: Colors.white),
+              style: TextStyle(fontSize: 16, color: Colors.white),
             )),
-        SizedBox(
+        const SizedBox(
           height: 20,
         )
       ],
@@ -163,20 +159,26 @@ class _TrainingPageState extends State<TrainingPage> {
 
     // This will show for showing result
     Container ResultPage = Container(
-      height: h*0.2,
-      width: w*0.2,
+      height: h * 0.2,
+      width: w * 0.2,
       child: Column(
         children: [
-          Text('Congratulations!', style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 26,
-            color: Colors.green,
-          ),),
-          Text('Your Score: $result', style: TextStyle(
-            fontSize: 18,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold,
-          ),),
+          const Text(
+            'Congratulations!',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 26,
+              color: Colors.green,
+            ),
+          ),
+          Text(
+            'Your Score: $result',
+            style: const TextStyle(
+              fontSize: 18,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ],
       ),
     );
@@ -243,26 +245,43 @@ class _TrainingPageState extends State<TrainingPage> {
               child: Column(
                 children: [
                   Center(
-                      child: Text(
-                    "Quiz",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      color: Colors.white,
-                    ),
-                  )),
-                  SizedBox(
+                    child: isPressed
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "Quiz",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              InkWell(
+                                onTap: ExitButton,
+                                child: Image.asset('assets/images/cancel.png'),
+                              )
+                            ],
+                          )
+                        : const Text(
+                            "Quiz",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: Colors.white,
+                            ),
+                          ),
+                  ),
+                  const SizedBox(
                     height: 10,
                   ),
-                  // Image(image: AssetImage('assets/images/quiz.jpg')),
                   QuizWidget,
                 ],
               ),
             ),
-
             // Resources contents
             isPressed
-                ? SizedBox()
+                ? const SizedBox()
                 : Container(
                     width: w * .95,
                     color: const Color(0xFF1D458B),
@@ -278,46 +297,32 @@ class _TrainingPageState extends State<TrainingPage> {
                           ),
                         )),
                         CustomContainer(
-                          pathName: 'assets/images/simg.jpg',
+                          pathName: 'assets/images/numpy.jpg',
                           partialText:
-                              'In Python, a module is a fundamental unit of code organization and reusability.',
+                              'NumPy, a cornerstone of Python data science, empowers you to work with numerical data like a pro. It excels...',
                           fullText:
-                              'In Python, a module is a fundamental unit of code organization and reusability. It\'s essentially a Python file (.py extension) containing a collection of definitions and statements.',
+                              'NumPy, a cornerstone of Python data science, empowers you to work with numerical data like a pro. It excels at creating and manipulating large, multidimensional arrays, making data handling a breeze. Need to crunch numbers? NumPy offers a built-in arsenal of mathematical functions and leverages optimized code for lightning-fast calculations. Plus, it seamlessly integrates with other data science libraries, forming a powerful workflow. The benefits are undeniable: speed through optimized computations, versatility for diverse data needs, and user-friendliness for smooth learning. By mastering NumPy, you unlock a gateway to the exciting world of data science in Python.  There are many resources available online to get you started, including official documentation, tutorials, and interactive courses. So, dive into NumPy and unleash the power of data analysis!',
+                        ),
+                        CustomContainer(
+                          pathName: 'assets/images/pytorch.jpg',
+                          partialText:
+                              'PyTorch is your key to building and training cutting-edge deep learning models in Python. Whether you\'re a...',
+                          fullText:
+                              'PyTorch is your key to building and training cutting-edge deep learning models in Python. Whether you\'re a beginner or a pro, PyTorch offers a user-friendly and flexible approach. Design neural networks with clear Python code, experiment with dynamic computational graphs for on-the-fly adjustments, and harness the power of GPUs for faster training. Backed by a thriving community, PyTorch provides extensive resources, tutorials, and pre-trained models to fuel your deep learning projects. What sets PyTorch apart? It\'s approachable for Python programmers, allows for flexible model architecture exploration, and prioritizes ease of use for research purposes. Dive into PyTorch and unlock the potential of artificial intelligence in your Python projects with the help of numerous online resources. Get started with the official website, tutorials, or even a deep learning course – the world of deep learning awaits!',
+                        ),
+                        CustomContainer(
+                          pathName: 'assets/images/numarray.jpeg',
+                          partialText:
+                              'Numarray, a predecessor to NumPy, was once a go-to library for numerical...',
+                          fullText:
+                              'Numarray, a predecessor to NumPy, was once a go-to library for numerical computations in Python. It offers similar functionalities for creating and handling multidimensional arrays. However, Numarray might be slower and have fewer mathematical functions. Stick to Numarray for existing code, but for new projects, NumPy is the way to go. It boasts superior performance, a wider range of features, and a more active developer community.',
                         ),
                         CustomContainer(
                           pathName: 'assets/images/simg.jpg',
                           partialText:
-                              'In Python, a module is a fundamental unit of code organization and reusability.',
+                              'Python is the undisputed champion for machine learning. Its clear and readable...',
                           fullText:
-                              'In Python, a module is a fundamental unit of code organization and reusability. It\'s essentially a Python file (.py extension) containing a collection of definitions and statements.',
-                        ),
-                        CustomContainer(
-                          pathName: 'assets/images/simg.jpg',
-                          partialText:
-                              'In Python, a module is a fundamental unit of code organization and reusability.',
-                          fullText:
-                              'In Python, a module is a fundamental unit of code organization and reusability. It\'s essentially a Python file (.py extension) containing a collection of definitions and statements.',
-                        ),
-                        CustomContainer(
-                          pathName: 'assets/images/simg.jpg',
-                          partialText:
-                              'In Python, a module is a fundamental unit of code organization and reusability.',
-                          fullText:
-                              'In Python, a module is a fundamental unit of code organization and reusability. It\'s essentially a Python file (.py extension) containing a collection of definitions and statements.',
-                        ),
-                        CustomContainer(
-                          pathName: 'assets/images/simg.jpg',
-                          partialText:
-                              'In Python, a module is a fundamental unit of code organization and reusability.',
-                          fullText:
-                              'In Python, a module is a fundamental unit of code organization and reusability. It\'s essentially a Python file (.py extension) containing a collection of definitions and statements.',
-                        ),
-                        CustomContainer(
-                          pathName: 'assets/images/simg.jpg',
-                          partialText:
-                              'In Python, a module is a fundamental unit of code organization and reusability.',
-                          fullText:
-                              'In Python, a module is a fundamental unit of code organization and reusability. It\'s essentially a Python file (.py extension) containing a collection of definitions and statements.',
+                              'Python is the undisputed champion for machine learning. Its clear and readable code makes building complex models a breeze. But Python\'s true power lies in its vast ecosystem of libraries. From data manipulation with NumPy and Pandas to algorithm implementation with Scikit-learn and deep learning frameworks like TensorFlow and PyTorch, Python offers tools for every stage of the ML pipeline. This versatility, coupled with a massive and supportive community, makes Python perfect for both beginners grasping core concepts and experts tackling advanced research. Dive into Python for ML with online courses, books like "Hands-On Machine Learning with Scikit-Learn, Keras & TensorFlow", or tutorials from libraries themselves. With Python by your side, you\'re ready to unlock the world of machine learning and its potential to solve real-world challenges.',
                         ),
                       ],
                     ),
